1e2d0afe6c195bebbb82f01d6914c1ed853d29b4
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-11-27 19:46:43 +0000

    up

--- js/swiper.js
+++ js/swiper.js
@@ -17,6 +17,42 @@ document.addEventListener("DOMContentLoaded", () => {
 					prevEl: ".zimmer-gallery-button-prev-0",
 				},
 			});
+			var zimmer02Swiper = new Swiper(".zimmer-gallery-1", {
+				loop: true,
+				parallax: true,
+				speed: 800,
+				autoplay: {
+					delay: 3000,
+				},
+				navigation: {
+					nextEl: ".zimmer-gallery-button-next-1",
+					prevEl: ".zimmer-gallery-button-prev-1",
+				},
+			});
+			var zimmer03Swiper = new Swiper(".zimmer-gallery-2", {
+				loop: true,
+				parallax: true,
+				speed: 800,
+				autoplay: {
+					delay: 3000,
+				},
+				navigation: {
+					nextEl: ".zimmer-gallery-button-next-2",
+					prevEl: ".zimmer-gallery-button-prev-2",
+				},
+			});
+			var zimmer04Swiper = new Swiper(".zimmer-gallery-3", {
+				loop: true,
+				parallax: true,
+				speed: 800,
+				autoplay: {
+					delay: 3000,
+				},
+				navigation: {
+					nextEl: ".zimmer-gallery-button-next-3",
+					prevEl: ".zimmer-gallery-button-prev-3",
+				},
+			});
 		}
 
 
