19e2172d760e6e4c7dea10704091df542443aa9e
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2024-06-14 15:57:55 +0100

    up

--- header.php
+++ header.php
@@ -359,7 +359,7 @@ if ( function_exists( 'icl_get_languages' ) ) {
 					}
 					foreach($remasterizeMMenu as $mID => $menuItem){
 						$activeItem = '';
-						$menuItemHTML = '<li id="li-'.$mID.'" class="item '.$activeItem.'">';
+						$menuItemHTML = '<li id="li-'.$mID.'" class="item '.$menuItem['active'].'">';
 						
 						# Do we have Children?
 						$childList = '';
